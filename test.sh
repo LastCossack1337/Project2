8ed0567c39d0280279ad572f311ee9f9d631de13
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-05-04 21:31:20 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+source ../../env.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /toxiccomments/input/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /toxiccomment/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../test-data/toxiccomment.py /toxiccomment/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./kmeans.py hdfs://$SPARK_MASTER:9000/kmeans/toxiccomment/
